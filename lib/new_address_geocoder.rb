3ed223104bdba61fa6e8c7c921e9cd103bf303ed
Carson Trent Long <ctlong.970@gmail.com>
2016-02-25 09:28:10 +0800

    new structure modelled after alexreisner/geocoder

---
+++ lib/new_address_geocoder.rb
@@ -0,0 +1,12 @@
+require 'address_geocoder/base'
+
+module NewAddressGeocoder # :nodoc:
+  
+  def self.valid_address?
+    # call out to base
+  end
+
+  def self.suggested_addresses
+    # call out to base
+  end
+end
