bdcec9dcd9dd6054ab52256dc7b74d095b94a4d4
Carson Long <ctlong.970@gmail.com>
2016-05-12 18:35:05 +0800

    feat(parser): refactored methods

--- lib/maps_api/google/parser.rb
+++ lib/maps_api/google/parser.rb
@@ -4,45 +4,59 @@ module MapsApi
   module Google
     # Class for parsing Google Maps API responses
     class Parser < ::AddressGeocoder::Parser
+      # List of Google's attribute title for streets
+      STREET_TYPES = { values: %w(route), name: 'street' }.freeze
       # List of Google's attribute title for cities
-      CITY_TYPES   = %w(neighborhood locality sublocality).freeze
+      CITY_TYPES   = { values: %w(neighborhood locality sublocality),
+                       name: 'city' }.freeze
       # List of Google's attribute titles for states
-      STATE_TYPES  = %w(administrative_area_level_4 administrative_area_level_3 administrative_area_level_2).freeze
+      STATE_TYPES  = { values: %w(administrative_area_level_4
+                                  administrative_area_level_3
+                                  administrative_area_level_2
+                                  administrative_area_level_1),
+                       name: 'state' }.freeze
       # List of Google's attribute titles for postal codes
-      POSTAL_TYPES = %w(postal_code postal_code_prefix).freeze
+      POSTAL_TYPES = { values: %w(postal_code postal_code_prefix),
+                       name: 'postal_code' }.freeze
 
       # Convert Google Maps' response into our format with the goal of finding
       # several matching addresses
       # @return (see AddressGeocoder::Parser#parse_response)
       def parse_response
-        @fields.each { |field| parse_field(field) }
+        @fields.each do |field|
+          parse_field(field)
+        end
         @addresses.delete(:switch)
-        [@addresses].flatten
+        @addresses = [@addresses].flatten
+        @addresses
       end
 
       # Takes a specific field and converts it into our format
       # @param field [Hash] one particular field from Google Maps' response
       # @return [void]
       def parse_field(field)
-        @field = field
-        if contains?(CITY_TYPES)
-          matched_city(field['long_name'])
-        elsif contains?(STATE_TYPES)
-          matched_state(field['long_name'])
-        elsif contains?(['administrative_area_level_1'])
-          matched_state(field['short_name'])
-        elsif contains?(POSTAL_TYPES)
-          @addresses[:postal_code] = field['long_name']
-        elsif contains?(['route'])
-          @addresses[:street] = field['long_name']
+        # if similar?(field['types'], CITY_TYPES)
+        #   matched_city(field['long_name'])
+        # elsif similar?(field['types'], STATE_TYPES)
+        #   matched_state(field['long_name'])
+        # elsif similar?(field['types'], ['administrative_area_level_1'])
+        #   matched_state(field['short_name'])
+        # elsif similar?(field['types'], POSTAL_TYPES)
+        #   @addresses[:postal_code] = field['long_name']
+        # elsif similar?(field['types'], ['route'])
+        #   @addresses[:street] = field['long_name']
+        # end
+        [STREET_TYPES, CITY_TYPES, STATE_TYPES, POSTAL_TYPES].each do |type|
+          if similar?(field['types'], type[:values])
+            send("add_#{type[:name]}", field)
+          end
         end
       end
 
       # Determine whether a specific value should be present or not in Google
       # Maps' response
       def self.value_present?(level, comparison_array, value)
-        # comparison_array.include?(level) && value
-        ([level] & comparison_array).any? && value
+        comparison_array.include?(level) && value
       end
 
       def self.just_country?(google_response)
@@ -57,29 +71,38 @@ module MapsApi
 
       private
 
-      def contains?(array)
-        (@field['types'] & array).any?
+      def similar?(array1, array2)
+        (array1 & array2).any?
       end
 
-      def matched_city(value)
+      def add_street(field)
+        @addresses[:street] = field['long_name']
+      end
+
+      def add_city(field)
         if @addresses[:city]
-          @addresses[:state]  = value
+          @addresses[:state]  = field['long_name']
           @addresses[:switch] = true
         else
-          @addresses[:city] = value
+          @addresses[:city] = field['long_name']
         end
       end
 
-      def matched_state(value)
-        if switched?
+      def add_state(field)
+        str = if field['types'].include? 'administrative_area_level_1'
+                'short_name'
+              else
+                'long_name'
+              end
+        if @addresses[:switch]
           @addresses[:city]   = @addresses[:state]
           @addresses[:switch] = false
         end
-        @addresses[:state] = value
+        @addresses[:state] = field[str]
       end
 
-      def switched?
-        @addresses[:switch]
+      def add_postal_code(field)
+        @addresses[:postal_code] = field['long_name']
       end
     end
   end
