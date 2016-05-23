c8d7a5b86412729b1e3a464340bc75eecfe48657
Carson Long <ctlong.970@gmail.com>
2016-05-23 12:51:37 +0800

    feat(spec): updated helper as per wing's suggestion

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,13 +5,13 @@ require 'pry'
 RSpec::Expectations.configuration.warn_about_potential_false_positives = false
 
 # set environment variable from .env
-begin
+if File.exist('.env')
   lines = File.read '.env'
   lines = lines.split('\n')
   lines.each do |line|
     line = line.split(' = ')
     ENV[line[0]] = line[1].gsub!('"', '')
   end
-rescue
+else
   puts 'no .env'
 end
