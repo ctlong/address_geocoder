db218ba3427693e131bd7de3547e071f4c800969
Carson Long <ctlong.970@gmail.com>
2016-05-23 12:54:19 +0800

    fixed typo

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,7 +5,7 @@ require 'pry'
 RSpec::Expectations.configuration.warn_about_potential_false_positives = false
 
 # set environment variable from .env
-if File.exist('.env')
+if File.exist?('.env')
   lines = File.read '.env'
   lines = lines.split('\n')
   lines.each do |line|
